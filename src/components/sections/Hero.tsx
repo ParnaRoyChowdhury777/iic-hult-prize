24ca70b77cee29cd3fb8689116b2e12e3112a2b8
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-21 22:58:20 +0530

    Fix styling issues in Hero component and HoverBorderGradient

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <Header />
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
       <Spotlight fill="#DB2777" className="top-0 ml-12 pl-12" />
-      
+
       <HoverBorderGradient>
         <span>HULT PRIZE</span>
       </HoverBorderGradient>
