57d9a832d32a03b6ed5ca2eb2227cb1307c87710
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-22 20:57:00 +0530

    Fix import path in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "../components/sections/Hero";
-import About from "@/components/sections/About";
+import About from "../components/sections/About";
 
 export default function Home() {
   return (
