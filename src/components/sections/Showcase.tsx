e66c1601cc5e449ead54a6a5f2877a10a058f2ed
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-04-24 01:47:53 +0530

    theme coloured

--- src/components/sections/Showcase.tsx
+++ src/components/sections/Showcase.tsx
@@ -3,7 +3,7 @@ import Show2024 from './Show2024'
 import Show2023 from './Show2023'
 const Showcase = () => {
   return (
-    <div className='bg-gray-50'>
+    <div className='bg-gray-50 dark:bg-slate-950'>
         <h1 className="text-center m-5 mb-10 text-5xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
           <span className=" text-transparent bg-clip-text bg-gradient-to-r to-pink-500 from-rose-600 text-5xl dark:text-white dark:text-current">
             SUBMISSIONS SHOWCASE
