f4c7b09bf9918d6b23e76e156d63cdfeb10b0f83
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-04-23 16:05:01 +0530

    show2024 base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/sections/Hero";
 import Show2024 from "@/components/sections/Show2024";
+import Show2023 from "@/components/sections/Show2023";
 import "./globals.css";
 export default function Home() {
   return (
   <>
     <Hero />;
     <Show2024 />;
+    <Show2023 />;
   </>
   );
 }
---
+++ src/components/sections/Show2023.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import Image from "next/image";
+import img1 from "../../images/2023theme.png";
+import Showcard from "../ui/showcard";
+import dummy from "../../images/dummy.png";
+
+const Show2023 = () => {
+  const cardsData = [
+    {
+      team: "Team 1",
+      tagline: "Tagline 1",
+      domain: "Domain 1",
+      imageSrc: dummy,
+      idea: "",
+    },
+    {
+      team: "Team 2",
+      tagline:
+        "Tagline 2",
+      domain: "Domain 2",
+      imageSrc: dummy,
+      idea: "",
+    },
+    {
+      team: "Team 3",
+      tagline: "Tagline 3",
+      domain: "Domain 3",
+      imageSrc: dummy,
+      idea: "",
+    },
+    {
+      team: "Team 4",
+      tagline: "Tagline 4",
+      domain: "Domain 4",
+      imageSrc: dummy,
+      idea: "",
+    },
+    {
+      team: "Team 5",
+      tagline: "Tagline 5",
+      domain: "Domain 5",
+      imageSrc: dummy,
+      idea: "",
+    },
+  ];
+  return (
+    <>
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="shadow-xl">
+          <a
+            href="#"
+            className="flex flex-col items-center bg-gray-100 border border-gray-300 rounded-xl shadow md:flex-row md:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-700"
+          >
+            <Image
+              className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg m-5"
+              src={img1}
+              alt="Hult Prize 2023"
+              width={150}
+              height={150}
+            />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-pink-600 dark:text-white text-center">
+                2023 HULT PRIZE CHALLENGE
+              </h5>
+              <h4 className="mb-2 text-xl font-bold tracking-tight text-gray-800 dark:text-white text-center">
+                THEME : REDESIGNING FASHION
+              </h4>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
+                Launch an innovative social venture in the clothing and fashion
+                industry to make it more sustainable.
+              </p>
+              <div className="text-center">
+                <a
+                  href="https://www.hultprize.org/2023-hult-prize-challenge/"
+                  className="inline-flex items-center font-medium text-pink-600 hover:text-pink-800 dark:text-pink-500 dark:hover:text-pink-700"
+                >
+                  Learn more
+                  <svg
+                    className=" w-2.5 h-2.5 ms-2 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center items-center">
+        {cardsData.map((cardData, index) => (
+          <Showcard
+            key={index} // Use the index as a key, or better yet, a unique identifier
+            team={cardData.team}
+            tagline={cardData.tagline}
+            domain={cardData.domain}
+            imageSrc={cardData.imageSrc as unknown as string} // Convert imageSrc to string
+            idea={cardData.idea}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+
+export default Show2023
