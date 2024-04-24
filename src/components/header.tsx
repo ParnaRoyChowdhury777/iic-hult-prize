5efc413789640649e137f916bce6289ac2c7f27e
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-25 02:10:53 +0530

    Update Header component and fix Showcase component styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
           About
         </Link>
         <a
-          href="/showcase"
+          href="/"
           className="font-normal hover:text-zinc-500 dark:hover:text-pink-600 hover:transition-all"
         >
           Winner
@@ -59,7 +59,7 @@ const Header = () => {
           </span>
         </span>
         <a
-          href="/"
+          href="/showcase"
           className="font-normal hover:text-zinc-500 dark:hover:text-pink-600 hover:transition-all"
         >
           Submissions
