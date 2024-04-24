91f3a6f5d3f8dcbba182fd339f80e34715c958d2
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-25 01:59:46 +0530

    Update Showcase component styling and add Header component

--- src/components/sections/Showcase.tsx
+++ src/components/sections/Showcase.tsx
@@ -9,7 +9,7 @@ const Showcase = () => {
       <Header />
       <div className="bg-gray-50 dark:bg-slate-950">
         <h1 className="text-center m-5 mb-10 text-5xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
-          <span className=" text-transparent bg-clip-text bg-gradient-to-r to-pink-500 from-rose-600 text-5xl dark:text-white dark:text-current">
+          <span className=" text-transparent bg-clip-text bg-gradient-to-r to-pink-500 from-rose-600 text-5xl dark:text-white dark:text-current mt-10">
             SUBMISSIONS SHOWCASE
           </span>{" "}
         </h1>
