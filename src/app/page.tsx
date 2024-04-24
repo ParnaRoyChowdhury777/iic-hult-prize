3756f01767e3cb150fc82f4588f9b07984f5a647
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-25 01:50:59 +0530

    Refactor Showcase component and remove unused import statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Hero from "../components/sections/Hero";
-import Showcase from "@/components/sections/Showcase";
-// import "./globals.css";
 import "./globals.css";
 export default function Home() {
   return (
   <>
     <Hero/>;
-    <Showcase/>;
   </>
 
   );
