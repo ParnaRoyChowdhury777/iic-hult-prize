7ab863364b8410d9a896874a86e0eb7b767caf52
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-25 01:46:46 +0530

    Refactor Showcase component and add Header component

--- src/components/sections/Showcase.tsx
+++ src/components/sections/Showcase.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import Show2024 from './Show2024'
-import Show2023 from './Show2023'
+import React from "react";
+import Show2024 from "./Show2024";
+import Show2023 from "./Show2023";
+import Header from "../header";
 const Showcase = () => {
   return (
-    <div className='bg-gray-50 dark:bg-slate-950'>
+    <>
+      <Header />
+      <div className="bg-gray-50 dark:bg-slate-950">
         <h1 className="text-center m-5 mb-10 text-5xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
           <span className=" text-transparent bg-clip-text bg-gradient-to-r to-pink-500 from-rose-600 text-5xl dark:text-white dark:text-current">
             SUBMISSIONS SHOWCASE
@@ -11,8 +14,9 @@ const Showcase = () => {
         </h1>
         <Show2024 />
         <Show2023 />
-    </div>
+      </div>
+    </>
   );
-}
+};
 
-export default Showcase
+export default Showcase;
