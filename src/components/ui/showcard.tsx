d97b71ed316a53080c3ae5131e5ad07adf4035ad
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-04-26 00:57:45 +0530

    css changes

--- src/components/ui/showcard.tsx
+++ src/components/ui/showcard.tsx
@@ -14,7 +14,7 @@ export default function showcard({ team, tagline, domain, imageSrc, idea, people
   
  
   return (
-    <div className="">
+    <div>
       <Card
         className="bg-pink-100 m-10 border-2 hover:shadow-lg shadow-lg shadow-pink-400/50 transform transition duration-500 ease-in-out hover:scale-105 hover:animate-colorChange cursor-pointer dark:bg-gray-300 "
         sx={{
