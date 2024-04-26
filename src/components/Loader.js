190b4ecb7bb4368bf533811ed95dea5e1c0a5c83
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-04-26 20:27:50 +0530

    Update preloader animation duration and theme in Loader.js and Hero.tsx

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,17 +5,14 @@ import Image from 'next/image';
 const Loading = () => {
     return (
         <div style={{
-            position: 'fixed',
             top: 0,
             left: 0,
             width: '100%',
             height: '100%',
-            backgroundColor: 'white',
-            display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             zIndex: 9999,
-        }}>
+        }} className="bg-white dark:bg-slate-950 fixed flex">
             <Image src={hult} alt='loader'/>
         </div>
     );
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
     
     setTimeout(() => {
       setIsLoading(false);
-    }, 1000); 
+    }, 1500); 
   }, []);
   return (
     <>
