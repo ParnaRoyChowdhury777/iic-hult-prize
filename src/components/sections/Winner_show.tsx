259a6344e36c2b7240803b78c45660ba27637c5c
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-04-26 22:12:27 +0530

    tooltip winner

--- src/components/sections/Winner_show.tsx
+++ src/components/sections/Winner_show.tsx
@@ -19,6 +19,40 @@ import winner1 from "../../../public/images/winner-1.jpg";
 import winner2 from "../../../public/images/winner-2.jpg";
 import winner3 from "../../../public/images/winner-3.jpg";
 import winner4 from "../../../public/images/winner-4.jpg";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+import { StaticImageData } from "next/image";
+ const people = [
+   {
+     id: 1,
+     name: "John Doe",
+     designation: "Software Engineer",
+     image:{avatar1},
+   },
+   {
+     id: 2,
+     name: "Robert Johnson",
+     designation: "Product Manager",
+     image:{avatar1},
+   },
+   {
+     id: 3,
+     name: "Jane Smith",
+     designation: "Data Scientist",
+     image:{avatar1},
+   },
+   {
+     id: 4,
+     name: "Emily Davis",
+     designation: "UX Designer",
+     image:{avatar1},
+   },
+   {
+     id: 5,
+     name: "Tyler Durden",
+     designation: "Soap Developer",
+     image:{avatar1},
+   },
+ ];
 
 const Winner_show = () => {
   
@@ -35,6 +69,7 @@ const Winner_show = () => {
             section.scrollIntoView({ behavior: 'smooth' });
         }
     };
+   
 
   return (
     <>
@@ -48,14 +83,13 @@ const Winner_show = () => {
         </div>
         <Card
           className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl border-zinc-300 border-spacing-2 dark:shadow-custom"
-          style={{ boxShadow: "0 12px 16px 0 rgba(252, 0, 125, 0.3)" }} 
+          style={{ boxShadow: "0 12px 16px 0 rgba(252, 0, 125, 0.3)" }}
         >
           <CardHeader className="flex justify-between items-center p-6">
             <CardTitle className="text-3xl font-semibold text-zinc-900">
-              Winner team name <br/>
+              Winner team name <br />
               <div className="flex justify-center tems-center">🏆</div>
             </CardTitle>
-            
           </CardHeader>
           <CardContent>
             <div className="flex items-center">
@@ -80,7 +114,7 @@ const Winner_show = () => {
             </div>
           </CardContent>
           <CardFooter className="flex justify-between items-center p-6">
-            <div className="flex -space-x-4 rtl:space-x-reverse">
+            {/* <div className="flex -space-x-4 rtl:space-x-reverse">
               <Image
                 className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
                 src={avatar1}
@@ -101,6 +135,9 @@ const Winner_show = () => {
                 src={avatar1}
                 alt=""
               />
+            </div> */}
+            <div className="flex flex-row items-center justify-center mb-3 w-full gap-3 ">
+              <AnimatedTooltip items={people.map(item => ({ ...item, image: item.image.avatar1 }))}/>
             </div>
             <div className="flex items-center">
               <button
@@ -113,74 +150,94 @@ const Winner_show = () => {
           </CardFooter>
         </Card>
         <section
-            id="imageSection"
-            className="flex flex-col flex-wrap md:flex-nowrap items-center justify-center mt-8 px-24 "
-            >
-            <p className="text-semibold text-center mx-auto mb-8 px-auto">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-                saepe exercitationem, quam quod asperiores praesentium rerum
-                molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
-                ratione porro sint magnam nam! Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Tempora dolore velit, laborum, nihil
-                ipsa ipsam deleniti officiis eveniet laboriosam dicta
-                exercitationem magni officia sed veniam libero suscipit at
-                voluptatum aspernatur.
+          id="imageSection"
+          className="flex flex-col flex-wrap md:flex-nowrap items-center justify-center mt-8 px-24 "
+        >
+          <p className="text-semibold text-center mx-auto mb-8 px-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            saepe exercitationem, quam quod asperiores praesentium rerum
+            molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
+            ratione porro sint magnam nam! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Tempora dolore velit, laborum, nihil
+            ipsa ipsam deleniti officiis eveniet laboriosam dicta exercitationem
+            magni officia sed veniam libero suscipit at voluptatum aspernatur.
+          </p>
+          <div className="grid grid-cols-5 gap-4 p-8 mx-auto">
+            <Image
+              src={winner1}
+              alt="Image 1"
+              className="col-span-3 h-50 w-50 rounded-lg border-2 border-pink-500"
+            />
+            <p className="col-span-2 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              saepe exercitationem, quam quod asperiores praesentium rerum
+              molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
+              ratione porro sint magnam nam! Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Beatae cumque at sit quasi
+              voluptatibus eos illo commodi! Veritatis cum porro eveniet?
+              Deleniti illum minus nostrum omnis maxime pariatur libero. Nam
+              impedit et minus rem, ex similique quia error at aliquam vel
+              exercitationem! Soluta rem ad sit! Recusandae voluptate quidem
+              facere? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quasi optio culpa magni esse ab itaque animi expedita
+              necessitatibus dolorem doloribus dicta amet assumenda facere
+              labore earum eius temporibus veniam, vero, explicabo molestiae!
+              Voluptatum repellendus, voluptates est eos facere laudantium
+              consequuntur quod quis optio quaerat asperiores sed accusantium
+              inventore dignissimos ipsa cum, omnis maiores fugit excepturi quia
+              delectus ratione? Quisquam, nemo!
             </p>
-            <div className="grid grid-cols-5 gap-4 p-8 mx-auto">
-                <Image
-                src={winner1}
-                alt="Image 1"
-                className="col-span-3 h-50 w-50 rounded-lg border-2 border-pink-500"
-                />
-                <p className="col-span-2 text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat, saepe exercitationem, quam quod asperiores praesentium
-                rerum molestiae, voluptatum nam hic voluptatibus. Omnis quo at
-                laborum ratione porro sint magnam nam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae cumque at sit quasi voluptatibus eos illo commodi! Veritatis cum porro eveniet? Deleniti illum minus nostrum omnis maxime pariatur libero. Nam impedit et minus rem, ex similique quia error at aliquam vel exercitationem! Soluta rem ad sit! Recusandae voluptate quidem facere? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi optio culpa magni esse ab itaque animi expedita necessitatibus dolorem doloribus dicta amet assumenda facere labore earum eius temporibus veniam, vero, explicabo molestiae! Voluptatum repellendus, voluptates est eos facere laudantium consequuntur quod quis optio quaerat asperiores sed accusantium inventore dignissimos ipsa cum, omnis maiores fugit excepturi quia delectus ratione? Quisquam, nemo!
-                </p>
-                <p className="col-span-1 text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat, saepe exercitationem, quam quod asperiores praesentium
-                rerum molestiae, voluptatum nam hic voluptatibus. Omnis quo at
-                laborum ratione porro sint magnam nam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione expedita, assumenda accusamus nostrum totam, consectetur quaerat accusantium excepturi cupiditate obcaecati est atque. lorem50
-                </p>
-                <Image
-                src={winner2}
-                alt="Image 2"
-                className="col-span-2 h-50 w-50 rounded-lg border-2 border-pink-500"
-                />
-                <Image
-                src={winner3}
-                alt="Image 3"
-                className="col-span-2 h-50 w-50 rounded-lg border-2 border-pink-500"
-                />
-                <Image
-                src={winner4}
-                alt="Image 4"
-                className="col-span-3 h-50 w-50 rounded-lg border-2 border-pink-500"
-                />
-                <p className="col-span-2 text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat, saepe exercitationem, quam quod asperiores praesentium
-                rerum molestiae, voluptatum nam hic voluptatibus. Omnis quo at
-                laborum ratione porro sint magnam nam!Lorem Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Voluptatum sunt rerum
-                facere, nihil vel necessitatibus? Debitis repellendus veritatis
-                consequatur a! Voluptatibus minima explicabo necessitatibus, ad
-                obcaecati molestiae dolore esse beatae. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime vel nisi aspernatur in autem earum facere quis dolorum ex commodi sit voluptate aut totam laborum, modi accusamus eligendi repellendus placeat beatae? Fuga recusandae, obcaecati laudantium in itaque omnis aut iste ad cumque similique ipsa. Nostrum earum aliquam aperiam recusandae vel!
-                </p>
-            </div>
-            <p className="text-semibold text-center mb-8">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-                saepe exercitationem, quam quod asperiores praesentium rerum
-                molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
-                ratione porro sint magnam nam! Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Tempora dolore velit, laborum, nihil
-                ipsa ipsam deleniti officiis eveniet laboriosam dicta
-                exercitationem magni officia sed veniam libero suscipit at
-                voluptatum aspernatur.
+            <p className="col-span-1 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              saepe exercitationem, quam quod asperiores praesentium rerum
+              molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
+              ratione porro sint magnam nam! Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Ratione expedita, assumenda
+              accusamus nostrum totam, consectetur quaerat accusantium excepturi
+              cupiditate obcaecati est atque. lorem50
             </p>
-            <Confetti width={1920} height={650} />
+            <Image
+              src={winner2}
+              alt="Image 2"
+              className="col-span-2 h-50 w-50 rounded-lg border-2 border-pink-500"
+            />
+            <Image
+              src={winner3}
+              alt="Image 3"
+              className="col-span-2 h-50 w-50 rounded-lg border-2 border-pink-500"
+            />
+            <Image
+              src={winner4}
+              alt="Image 4"
+              className="col-span-3 h-50 w-50 rounded-lg border-2 border-pink-500"
+            />
+            <p className="col-span-2 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              saepe exercitationem, quam quod asperiores praesentium rerum
+              molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
+              ratione porro sint magnam nam!Lorem Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Voluptatum sunt rerum facere, nihil
+              vel necessitatibus? Debitis repellendus veritatis consequatur a!
+              Voluptatibus minima explicabo necessitatibus, ad obcaecati
+              molestiae dolore esse beatae. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Maxime vel nisi aspernatur in autem
+              earum facere quis dolorum ex commodi sit voluptate aut totam
+              laborum, modi accusamus eligendi repellendus placeat beatae? Fuga
+              recusandae, obcaecati laudantium in itaque omnis aut iste ad
+              cumque similique ipsa. Nostrum earum aliquam aperiam recusandae
+              vel!
+            </p>
+          </div>
+          <p className="text-semibold text-center mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            saepe exercitationem, quam quod asperiores praesentium rerum
+            molestiae, voluptatum nam hic voluptatibus. Omnis quo at laborum
+            ratione porro sint magnam nam! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Tempora dolore velit, laborum, nihil
+            ipsa ipsam deleniti officiis eveniet laboriosam dicta exercitationem
+            magni officia sed veniam libero suscipit at voluptatum aspernatur.
+          </p>
+          <Confetti width={1920} height={650} />
         </section>
       </section>
     </>
